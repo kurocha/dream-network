2be1d32338e771e5b7373afa6d4177692d87c08b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-07-16 01:17:42 +1200

    Initial migration of code from old Dream monolithic repo.

---
+++ source/Dream/Network/Network.hpp
@@ -0,0 +1,55 @@
+//
+//  Network/Network.h
+//  This file is part of the "Dream" project, and is released under the MIT license.
+//
+//  Created by Samuel Williams on 17/11/07.
+//  Copyright (c) 2007 Samuel Williams. All rights reserved.
+//
+//
+
+#pragma once
+
+#include <Dream/Framework.hpp>
+
+#include <Dream/Core/Endian.hpp>
+#include <Dream/Core/Data.hpp>
+#include <Dream/Events/Loop.hpp>
+
+#include <exception>
+#include <stdexcept>
+
+namespace Dream {
+	/// Network related functions for clients and servers.
+	namespace Network {
+		/** Indicates that a connection error of some sort has occurred.
+
+		 */
+		class ConnectionError : public std::runtime_error {
+		public:
+			explicit ConnectionError (const std::string & what);
+		};
+
+		/** Indicates that the socket has disconnected, typically by error (e.g. phsysical disconnect).
+
+		 @sa Socket::close
+		 */
+		class ConnectionResetByPeer : public ConnectionError {
+		public:
+			explicit ConnectionResetByPeer (const std::string & what);
+		};
+
+		/** Indiciates that the operation cannot complete because the pipe has been shutdown.
+
+		 If you attempt to read from the remote end and it has been shutdown, this error will be thrown.
+
+		 @sa Socket::shutdown
+		 */
+		class ConnectionShutdown : public Events::FileDescriptorClosed {
+		protected:
+			const std::string & _what;
+
+		public:
+			explicit ConnectionShutdown (const std::string & what);
+		};
+	}
+}
---
+++ teapot.rb
@@ -0,0 +1,57 @@
+
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+teapot_version "1.0"
+
+define_target "dream-network" do |target|
+	target.build do |environment|
+		source_root = target.package.path + 'source'
+		
+		copy headers: source_root.glob('Dream/**/*.hpp')
+		
+		build static_library: "DreamNetwork", source_files: source_root.glob('Dream/**/*.cpp')
+	end
+	
+	target.depends :platform
+	target.depends "Language/C++11"
+	
+	target.depends "Build/Files"
+	target.depends "Build/Clang"
+
+	target.depends "Library/DreamEvents"
+	target.depends "Library/Dream"
+	
+	target.provides "Library/DreamNetwork" do
+		append linkflags "-lDreamNetwork"
+	end
+end
+
+define_target "dream-network-tests" do |target|
+	target.build do |environment|
+		test_root = target.package.path + 'test'
+		
+		run tests: "DreamNetwork", source_files: test_root.glob('Dream/**/*.cpp')
+	end
+	
+	target.depends "Library/UnitTest"
+	target.depends "Library/DreamNetwork"
+
+	target.provides "Test/DreamNetwork"
+end
+
+define_configuration "test" do |configuration|
+	configuration[:source] = "https://github.com/kurocha"
+	
+	configuration.require "platforms"
+	configuration.require "build-files"
+	
+	configuration.require "dream"
+	configuration.require "dream-events"
+	
+	configuration.require "euclid"
+	configuration.require "unit-test"
+	
+	configuration.require "language-cpp-class"
+end
